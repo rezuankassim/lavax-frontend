14a8b17e4a236f79b671e36ac914c8c33d869772
Rezuan Kassim <rezuan@easyparcel.com>
2023-01-20 15:44:46 +0800

    update image domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "lavax-backend.rezuankassim.com"],
+    domains: ["localhost", "lavax-rezuankassim.com"],
   },
 };
 
